eeb1edc50119368dac361287e120ce830567aa08
Jandy <reverdy@protonmail.com>
2019-09-09 23:12:17 -0700

    buttons created for added topics

--- javascript/gif.js
+++ javascript/gif.js
@@ -7,9 +7,24 @@ $(document).ready(function() {
     "fifth element"
   ];
 
-  for (let i = 0; i < topics.length; i++) {
-    var topicBtn = $("<button>");
-    topicBtn.text(topics[i]);
-    $("#buttons-view").append(topicBtn);
+  function showButtons() {
+    $("#buttons-view").empty();
+
+    for (let i = 0; i < topics.length; i++) {
+      var topicBtn = $("<button>");
+      topicBtn.addClass("gif");
+      topicBtn.attr("data-name", topics[i]);
+      topicBtn.text(topics[i]);
+      $("#buttons-view").append(topicBtn);
+    }
   }
+
+  $("#add-gif").on("click", function() {
+    event.preventDefault();
+    var gif = $("#gif-input")
+      .val()
+      .trim();
+    topics.push(gif);
+    showButtons();
+  });
 });
