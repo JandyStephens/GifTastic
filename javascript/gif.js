9ce3344164c3e3836beeb6f15f9eae85ba9372e2
Jandy <reverdy@protonmail.com>
2019-09-09 22:34:03 -0700

    input form and buttons of array showing

--- javascript/gif.js
+++ javascript/gif.js
@@ -1,1 +1,15 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  var topics = [
+    "gardening",
+    "dwarf hamsters",
+    "futurama",
+    "cake decorating",
+    "fifth element"
+  ];
+
+  for (let i = 0; i < topics.length; i++) {
+    var topicBtn = $("<button>");
+    topicBtn.text(topics[i]);
+    $("#buttons-view").append(topicBtn);
+  }
+});
